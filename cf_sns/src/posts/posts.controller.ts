8ba1ce1a92a7b9e2e747f410e52e23b95fbc8a04
owencity <skyko6530@naver.com>
2024-11-11 22:47:55 +0900

    nest js 서비스 로직으로 리팩토링

--- cf_sns/src/posts/posts.controller.ts
+++ cf_sns/src/posts/posts.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Post, Put } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
 /*
@@ -8,26 +8,70 @@ import { PostsService } from './posts.service';
     likeCount: number;
     commentCount: number;
   */
-interface Post {
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
+  // 1) Get /posts
+  // 모든 post를 다 가져온다.
   @Get()
-  getPost(): Post {
-    return {
-      author: 'newjeans_official',
-      title: '뉴진스 민지',
-      content: '메이크업 고치고 있는 민지',
-      likeCount: 10000000,
-      commentCount: 9999999,
-    };
+  getPosts() {
+    return this.postsService.getAllPosts();
   }
+
+  // 2) GET /posts/:id
+  // id에 해당되는 post를 가져온다.
+  // 예를 들어서 id=1일 경우 id가 1인 포스트를 가져온다.
+  @Get(':id')
+  getPost(@Param('id') id: string) {
+   return this.postsService.getPostById(+id);
+  }
+  // 3) Post /posts
+  // POST를 생성한다.
+  @Post()
+  postPosts(
+    @Body('author') author:string,
+    @Body('title') title:string,
+    @Body('content') content:string,
+  ){
+    return this.postsService.createPost(
+      author, title, content
+    )
+  }
+
+  // 4) PUT /posts/:id
+  @Put(':id')
+  putPost(
+    @Param('id') id:string,
+    @Body('author') author?:string,
+    @Body('title') title?:string,
+    @Body('content') content?:string,
+  ) {
+    return this.postsService.updatePost(
+      +id, author, title, content,
+    );
+  }
+
+  // 5) Delete /posts/:id
+
+  @Delete(':id')
+  deletePost(
+    @Param('id') id: string,
+  ) {
+    return this.postsService.deletePost(+id);
+  }
+
+  // id
+  // @Get()
+  // getPost(): PostModel {
+  //   return {
+  //     id: 1,
+  //     author: 'newjeans_official',
+  //     title: '뉴진스 민지',
+  //     content: '메이크업 고치고 있는 민지',
+  //     likeCount: 10000000,
+  //     commentCount: 9999999,
+  //   };
+  // }
 }
--- cf_sns/src/posts/posts.service.ts
+++ cf_sns/src/posts/posts.service.ts
@@ -1,4 +1,101 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+export interface PostModel {
+    id: number;
+    author: string;
+    title: string;
+    content: string;
+    likeCount: number;
+    commentCount: number;
+  }
+  
+  let posts : PostModel[] = [
+    {
+      id: 1,
+      author: 'newjeans_official',
+      title: '뉴진스 민지',
+      content: '메이크업 고치고 있는 민지',
+      likeCount: 1000000,
+      commentCount: 999999,
+    },
+    {
+      id: 2,
+      author: 'newjeans_official',
+      title: '뉴진스 해린',
+      content: '노래 연습하고 있는 해린',
+      likeCount: 1000000,
+      commentCount: 999999,
+    },
+    {
+      id: 3,
+      author: 'blackpink_official',
+      title: '뉴진스 해린',
+      content: '공연하고 있는 로제',
+      likeCount: 1000000,
+      commentCount: 999999,
+    },
+  ];
+
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+    getAllPosts() {
+        return posts
+    }
+
+    getPostById(id: number) {
+        const post = posts.find((post) => post.id === +id); // + -> 암묵적인 자바스크립트의 형변환 , +는 숫자가아닌 경우 숫자로 변환하려고 시도
+        // 문자열이 변환가능한 숫자인경우 변환, 아닌경우 NaN의 결과를 냄 number(id) 와 같은 기능을한다.
+        if(!post) {
+          throw new NotFoundException();
+        }
+        return post;
+    }
+
+    createPost(author:string, title: string, content: string) {
+        const post : PostModel = {
+            id: posts[posts.length -1].id + 1,
+            author,
+            title,
+            content,
+            likeCount: 0,
+            commentCount: 0,
+          };
+          posts = [
+            ...posts,
+            post,
+          ];
+          return post;
+    }
+
+    updatePost(postId: number, author: string, title: string, content: string) {
+        const post = posts.find(post => post.id === postId);
+
+        if(!post){
+          throw new NotFoundException();
+        }
+        if(author) {
+          post.author = author;
+        }
+        if(title) {
+          post.title = title;
+        }
+        if(content) {
+          post.content = content;
+        }
+        posts = posts.map(prevPost => prevPost.id === postId ? post: prevPost);
+        return post;
+    }
+
+    deletePost(postId: number) {
+        const post = posts.find((post) => post.id === postId);
+
+    if(!post) {
+      throw new NotFoundException();
+    }
+
+    posts = posts.filter(post => post.id !== postId);
+
+    return postId;
+    }
+}
