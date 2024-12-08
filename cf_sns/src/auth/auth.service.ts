464920d94e20314924cf2b04c4931447a0130721
owencity <skyko6530@naver.com>
2024-12-08 23:16:27 +0900

    코드정리

--- cf_sns/src/auth/auth.service.ts
+++ cf_sns/src/auth/auth.service.ts
@@ -50,16 +50,17 @@ export class AuthService {
     extractTokenFromHeader(header: string, isBearer: boolean) {
     
     const splitToken = header.split(' ') // 띄어쓰기 기준으로 나눠 리스트로 반환 
-    
+    // 헤더에서 추출한 Bearer <JWT> -> [Bearer or Basic , JWT 값] 반환
     const prefix = isBearer ? 'Bearer' : 'Basic';
-
+    
     if(splitToken.length !== 2 || splitToken[0] !== prefix ) {
         throw new UnauthorizedException('잘못된 토큰입니다.');
     }
-
+    // 추출한 JWT 배열값이 2개가 아니라면 잘못된값 그리고 들어온 JwT값이 prefix와 동일하지않다면 잘못된값 
     // 클라이언트에서 항상 잘못된 값이 들어올수있다는 것을 항상 가정해야한다. 
+    
     const token = splitToken[1];
-
+    // 1번째 index값이 token 값이니 1번인덱스로 반환
     return token;
 }
 
@@ -116,6 +117,23 @@ signToken(user: Pick<UsersModel, 'email' | 'id'>, isRefreshToken: boolean) {
         }
     }
 
+    /*  
+        첫 로그인시 accessToken 과 refresh 토큰 같이 반환
+        accessToken은 refresh 토큰이 아니므로 false 
+        refreshToken 은 true
+        만료시 로그아웃
+        refresh는 만료시간을 길게줘서 httpOnly 쿠키로 저장 
+        XSS 공격방지를 위해 권장 
+        서버는 클라이언트로부터 받은 refreshToken 의 서명,유효시간을 확인 할 뿐,
+        별도 상태 저장하지않는다. 
+        
+        탈치 방지 전략
+        최대한 짧은 유효시간
+        Refresh 토큰 요청시 , 사용자 IP 주소 저장, 변경 여부 확인
+        TLS/HTTPS 필수적 사용
+        HTTP 헤더로만 확인할 수 있게(클라이언트 접근불가)
+    */
+
     async authenticateWithEmailAndPassword(user: Pick<UsersModel , 'email' | 'password'> )  {
         /* 
             1. 사용자가 존재하는지 확인 (email)
@@ -160,6 +178,11 @@ signToken(user: Pick<UsersModel, 'email' | 'id'>, isRefreshToken: boolean) {
             return this.loginUser(newUser);
         }
 
+        /* 
+            hash(user패스워드값, 해쉬라운드 설정)
+            이후 회원가입성공시 바로 로그인
+        */
+
         decodeBasicToken(base64String: string)  {
             const decoded = Buffer.from(base64String, 'base64').toString('utf-8');
             const split = decoded.split(':');
@@ -177,11 +200,80 @@ signToken(user: Pick<UsersModel, 'email' | 'id'>, isRefreshToken: boolean) {
             }
         }
 
+        /* 
+            위의 비밀번호 관련은 임시로 간소화한 방법이며
+            보통 basic을 디코딩하지않고 bcrypt.compare 를써서
+            로그인 시 저장된 해시값에서 솔트를 추출, 들어온 비밀번호와 동일한 방식으로 계산
+            (해싱)한 뒤, 전체 해시값을 바이트 단위로 비교
+            이 과정에서 원문 비밀번호를 직접 확인하지않습니다.
+
+            해시값은 원래 문자열이 아닌 바이너리 데이터, 문자열 비교는 시스템별 차이(예: 문자셋, 인코딩)로 문제가
+            생길 수 있지만, 바이트 비교는 항상 일관된 결과를 보장한다.
+            또한 , 정확성을 가질수 있다. 저장된 해시값과 새로 생성된 해시값이 완전히 동일한지 확인할 수 있다.
+
+            비밀번호의 목적을 생각해보면 쉽다.
+            비밀번호의 검증의 목적은 입력된 비밀번호가 원래의 비밀번호가 같은지 확인만 하는것
+            그러므로 원문 비밀번호를 저장하거나 디코딩 할 필요가없다.
+
+            단방향 함수-> 입력값을 출력값으로 변환은쉽다, 출력값을 입력값을 역산출하는 것은 어렵다
+
+            단반향 함수 특성
+            1. 의도적인 정보 손실:
+            해싱함수는 입력데이터를 고정된 길이이 해시값으로 변환
+            1기가라도해도 예로 SHA-256인경우 256비트로 고정된다. 이 과정에서 일부 데이터 정보가 손실되므로
+            원본을 복원할수 없다.
+            2. 비결정성
+            같은 비밀번호라도 다른 솔트를 사용할 경우 완전히 다른 해시값이 생성된다.
+            즉 , 출력값만으로 어떤 솔트를 사용했는지 알 수없다
+
+            역산출의 계산 복잡도
+            입력값을 알아내려면 가능한 모든 조합을 시도 해야한다 
+            -> 예로 비밀번호가 8자리 일시 (대소문자, 숫자, 특문 포함) 가능한 조합은 약 6천조 이다.
+            그래서 최대한 사용자에게 비밀번호를 어렵게 만들도록 유도하는것
+            작은 입력값에도 큰 연산 비용
+            bcrypt 게산비용을 증가시키기 위해 솔트 라운드 사용
+            솔트라운드가 10일경우 , 해싱과정을 2의10제곱(1024)의 반복 계산을 수행하므로 , 역산출에 드는 비용이 매우크다
+
+            솔트라운드는 해싱 과정에서 계산을 반복적으로 수행하는 횟수를 지정하는 값
+            이값이 클수록 해싱과정이 느려지며, 보안성이 높아짐
+
+            의도적으로 해싱작업에 더많은 계산을 요구
+            비밀번호의 해싱의 시간복잡도 제어 -> 예로 10일때 걸리는 시간 1ms, 12일때 약 4ms로 증가
+
+            반복계산의 과정은 
+            1. 솔트값 생성 : bcrypt는 무작위로 생성한 솔트를 비밀번호에 추가
+            2. 첫번째 해싱 : 비밀번호와 솔트를 결합하여 첫 번째 해시값을 생성
+            3. 반복 해싱 : 첫버째 해싱결과를 입력값으로사용 , 총2의10제곱 번의 반복 계산을 수행
+            각 반복에서 이전 단계의 해시값을 기반으로 새 해시값을 생성
+            4. 최종 해시값 생성 : 2의10제곱번 반복이 완료되면, 최종 해시값을 반환, 
+
+            솔트는 해싱과정에서 입력값(비밀번호)에 추가되는 임의의 고유데이터 
+            같은 비밀번호라도 서로다른 해시값을 생성하기위해 사용
+            또한 무차별 대입 공격방지 (레인보우테이블 활용)하는것을 어렵게 만듬 
+            bcrypt는 솔트값을 자동으로 생성, 이값을 최종 해시에 포함 합니다.
+            개발자가 별도로 솔트를 생성하거나 관리할 필요가없다.
+
+            최종 결과에는 솔트값도 포함
+
+            암호화하는 데이터를 인코딩하고, 키를 사용해 원본데이터를 복원할 수 있다.
+            해싱은 원본 데이터를 복원할 수 없도록 설계된 단방향 함수이다.
+            설계 철학 자체가 "역산출이 불가능하도록" 만든다.
+        */
+
         verifyToken(token: string) {
             try {
                 return this.jwtService.verify(token, {
+                // verify<T extends object = any>(token: string, options?: JwtVerifyOptions): T;   
+                /* 
+                    verify는 JWT를 검증하는 함수 , 주어진 JWT토큰이 유효한지 확인, 서명을 검증하여 유효되지않거나 만료된
+                    토큰에 대해 예외를 발생
+                    token :string -> 검증할 jwt 문자열 
+                    options? : JwtVerifyOptions -> 선택적으로 제공되는 옵션(예: 토큰 알고리즘, 발행자(issuer), 대상(audience) 등)
+                    T : 토큰에 포함된 페이로드를 반환 , 페이로드는 보통 사용자 정봑 권한등을 담고있음
+                */
                     secret: this.configService.get<string>(ENV_JWT_SECRET_KEY),
                 });
+                // jwtService의 verify 활용 
             } catch (e) {
                 throw new UnauthorizedException('토큰이 만료됬거나 잘못된 토큰입니다.');
             }
--- cf_sns/src/auth/guard/socket/socket-bearer-token.guard.ts
+++ cf_sns/src/auth/guard/socket/socket-bearer-token.guard.ts
@@ -4,41 +4,70 @@ import { Observable } from "rxjs";
 import { AuthService } from "src/auth/auth.service";
 import { UsersService } from "src/users/users.service";
 
-@Injectable()
+@Injectable() // SocketBearerTokenGuard를 provider로 만들기위함, 다른곳에 의존성 주입할수 있도록
 export class SocketBearerTokenGuard implements CanActivate {
+    // CanActivate -> NestJS의 Guard를 구현하기 위한 인터페이스
     constructor(
         private readonly authService: AuthService,
         private readonly userService: UsersService,
     ) {}
 
-    async canActivate(context: ExecutionContext):   Promise<boolean> {
-        const socket = context.switchToWs().getClient();
-
-        const headers = socket.handshake.headers;
-
-        // Bearer 
-        const rawToken = headers['authorization'];
-
-        if(!rawToken) {
-            throw new WsException('토큰이 없습니다!');
-        }
-        try {
-            const token = this.authService.extractTokenFromHeader(
-                rawToken,
-                true,
-            );
+    /* 
+        private readonly 쓰는 이유?
+        OOP 원칙을 따르고, 코드의 안정성과 가독성을 높이기 위함 
+        private : 캡슐화 , readonly : 초기화 이후 변경불가(불변성)
+        의도 명확화 -> 이속성이 외부에 노출되지않은며, 클래스 내부에서도 수정되지 않는다는 의도를 "명학"히 전달
+    */
     
-            const payload = this.authService.verifyToken(token);
-            const user = await this.userService.getUsersByEmail(payload.email);
+        async canActivate(context: ExecutionContext):   Promise<boolean> { 
+            
+            /* 
+                ExecutionContext는 NestJS에서 제공하는 실행 컨텍스트 객체,
+                현재 실행중인 요청과 관련된 정보를 제공하는 역할
+                (예: HTTP, WebSocket 등 )
+            */
+            const socket = context.switchToWs().getClient();
+            
+            const headers = socket.handshake.headers;
+            /* 
+                웹소켓 연결과정에서 http 핸드쉐이크 요청에 포함된 헤더정보를 가져오는 것 
+                핸드쉐이크 이후 웹소켓 동작하는데 양방향 통신이 시작되고
+                이 시점 이후에는 헤더 정보를 전송할 수없습니다. 
+                -> WebSocket 메시지에는 헤더개념이 없으며, 메시지 페이로드에 데이터를 포함시켜야합니다.
+                메시지 페이로드에 포함시킬수는 있지만 보안에 위험을 야기할수있으므로 권장하지 않는다.
+                페이로드는 네트워크 상에서 쉽게 노출될수 있고 재전송의 위험이 있다.
+                */
+
+
+            // Bearer 
+            const rawToken = headers['authorization'];
+            /* 
+                브래킷 표기법, 객체 프로퍼티 접근 방식중 하나 
+                headers 객체에 속한 authorization 키의 값을 가져온다.
+            */
     
-            socket.user = user;
-            socket.token = token;
-            socket.tokenType = payload.tokenType;
+            if(!rawToken) {
+                throw new WsException('토큰이 없습니다!');
+            }
+            try {
+                const token = this.authService.extractTokenFromHeader(
+                    rawToken,
+                    true,
+                );
         
-            return true;
-        } catch (e) {
-            throw new WsException('토큰이 유효하지 않습니다.')
+                const payload = this.authService.verifyToken(token);
+                // token 에 포함된 페이로드 반환
+                const user = await this.userService.getUsersByEmail(payload.email);
+                // payload의 email 값을 통해 지금의 user 의 email값 반환
+                socket.user = user;
+                socket.token = token;
+                socket.tokenType = payload.tokenType;
+            
+                return true;
+            } catch (e) {
+                throw new WsException('토큰이 유효하지 않습니다.')
+            }
+          
         }
-      
-    }
+    
 }
